5457d39b638ca0daac83980623b346fec2c3e894
MissisA <111286681+MissisA@users.noreply.github.com>
2025-07-05 14:20:16 +0700

    new commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
